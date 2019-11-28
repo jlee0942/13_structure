4537a3d1f45198f32a5ec1513d6eaff63e6044c4
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2019-11-28 16:12:00 +0900

    02_1

--- main.c
+++ main.c
@@ -2,21 +2,80 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct student{
-	int 	ID;
-	char	name[10];
-	double	grade;
-};
+
+int is_whitespace(char c){
+	if( c== ' '||
+		c=='\n'||
+		c=='\r'||
+		c=='\t'||
+		c=='(')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int fget_word(FILE* fp, char *word){
+	char c;
+	int cnt;
+	
+	while ((c=fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(c)==0);
+			break;
+	}
+	
+	if(c==EOF)
+	{
+		return 0;
+	}
+	
+	cnt=0;
+	word[cnt++]=c;
+	word[cnt]='\0';
+	
+	while ((word[cnt]=fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(word[cnt])==1)
+		{
+			word[cnt]='\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;
+}
 
 int main(int argc, char *argv[]) {
-	struct student s1 = {24, "Jihyun", 4.3};
-	s1.ID=123456;
-	strcpy(s1.name, "Juyeop");
-	s1.grade=4.2;
 	
-	printf("ID:%d\n", s1.ID);
-	printf("name:%s\n", s1.name);
-	printf("grade:%f\n", s1.grade);
-		
+	FILE *fp; 
+	char fpath[100];
+	char word[100];
+	
+	//file open
+	printf("input file path : ");
+	scanf("%s", &fpath);
+	fp = fopen(fpath,"r");
+	
+	if(fp==NULL)
+	{
+		printf("invaild file path");
+		return -1;
+	}
+
+	
+	//word reading&analysis
+	while((fget_word(fp,word))!=0)
+	{
+		printf("%s\n", word);
+	}
+	
+	
+	//output
+	fclose(fp);
 	return 0;
 }
